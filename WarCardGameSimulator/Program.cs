2f09c675c99e88c8648b30e80105e20c4e207cd5
MHT <malikhejle@gmail.com>
2018-08-05 23:48:31 +0200

    initial

---
+++ WarCardGameSimulator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WarCardGameSimulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
